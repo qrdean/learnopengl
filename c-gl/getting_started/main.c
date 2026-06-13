75a8a86b682db58dc2bb43e9942a732b6604e92c
quinton dean <dean.quinton@ymail.com>
2026-06-13 18:21:32 -0500

    move to folder before going to lighting

--- c-gl/getting_started/main.c
+++ c-gl/getting_started/main.c
@@ -1,12 +1,38 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
-// #include <math.h>
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <cglm/cglm.h>
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
 
+vec3 cameraPos = (vec3){0.0f, 0.0f, 3.0f};
+vec3 cameraFront = (vec3){0.0f, 0.0f, -1.0f};
+vec3 cameraUp = (vec3){0.0f, 1.0f, 0.0f};
+float deltaTime = 0.0f;
+float lastFrame = 0.0f;
+
+void pprint_vec2(vec2 v) 
+{
+  printf("vector: [%f %f]", v[0], v[1]);
+}
+
+
+void pprint_vec3(vec3 v, const char* msg) 
+{
+  printf("\n[%s] vector: \n[%f %f %f]\n", msg, v[0], v[1], v[2]);
+}
+
+void pprint_vec4(vec4 v) 
+{
+  printf("vector: [%f %f %f %f]", v[0], v[1], v[2], v[3]);
+}
+
+void pprint_mat2(mat2 m, const char* name) 
+{
+  printf("\nmatrix %s: \n[%f %f]\n[%f %f]\n", name, m[0][0], m[0][1], m[1][0], m[1][1]);
+}
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height) 
 {
@@ -18,6 +44,37 @@ void processInput(GLFWwindow *window)
   if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) 
   {
     glfwSetWindowShouldClose(window, 1);
+  }
+
+  float camSpeed = 0.05f * deltaTime;
+  if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS) 
+  {
+    vec3 temp = {0.0f, 0.0f, 0.0f};
+    glm_vec3_muladds(cameraFront, camSpeed, temp);
+    glm_vec3_add(cameraPos, temp, cameraPos);
+  }
+
+  if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS) 
+  {
+    vec3 temp = {0.0f, 0.0f, 0.0f};
+    glm_vec3_muladds(cameraFront, camSpeed, temp);
+    glm_vec3_sub(cameraPos, temp, cameraPos);
+  }
+
+  if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS) 
+  {
+    vec3 temp = {0.0f, 0.0f, 0.0f};
+    glm_vec3_crossn(cameraFront, cameraUp, temp);
+    glm_vec3_muladds(temp, camSpeed, temp);
+    glm_vec3_sub(cameraPos, temp, cameraPos);
+  }
+
+  if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS) 
+  {
+    vec3 temp = {0.0f, 0.0f, 0.0f};
+    glm_vec3_crossn(cameraFront, cameraUp, temp);
+    glm_vec3_muladds(temp, camSpeed, temp);
+    glm_vec3_add(cameraPos, temp, cameraPos);
   }
 } 
 
@@ -161,6 +218,7 @@ void loadTexture(const char* filename)
   stbi_image_free(data);
 }
 
+
 // const char* vertex_shader_source =
 //   "#version 330 core\n"
 //   "layout (location = 0) in vec3 aPos;\n"
@@ -286,7 +344,6 @@ int main(void)
   glGenTextures(1, &texture1);
   glBindTexture(GL_TEXTURE_2D, texture1);
 
-
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
@@ -357,26 +414,42 @@ int main(void)
   //
 
   glUseProgram(s.ID);
+  // setup textures
   glUniform1i(glGetUniformLocation(s.ID, "texture1"), 0);
   glUniform1i(glGetUniformLocation(s.ID, "texture2"), 1);
-  // setShaderInt(s, "texture1", 0);
-  // setShaderInt(s, "texture2", 1);
-  //
-  // mat4 trans = GLM_MAT4_IDENTITY;
-  // glm_translate(trans, (vec3){1.0f, 1.0f, 0.0f});
-  //
-  // vec4 orthProjMatrix;
-  // glm_ortho(0.0f, 800.0f, 0.0f, 600.0f, 0.1f, 100.0f, &orthProjMatrix);
-  // printf("orthProjMatrix: [%f, %f, %f, %f]", orthProjMatrix[0], orthProjMatrix[1], orthProjMatrix[2], orthProjMatrix[3]);
-  //
+
   mat4 model = GLM_MAT4_IDENTITY_INIT;
   glm_rotate(model, glm_rad(-55.0f), (vec3){1.0f, 0.0f, 0.0f});
 
   mat4 view = GLM_MAT4_IDENTITY_INIT;
-  glm_translate(view, (vec3){0.0f, 0.0f, -3.0f});
+  // glm_translate(view, (vec3){0.0f, 0.0f, -3.0f});
+  glm_lookat((vec3){0.0f, 0.0f, 3.0f}, (vec3){0.0f, 0.0f, 0.0f}, (vec3){0.0f, 1.0f, 0.0f}, view);
 
   mat4 proj;
   glm_perspective(glm_rad(45.0f), 800.0f/600.0f, 0.1, 100.0f, proj);
+  int projectionLoc = glGetUniformLocation(s.ID, "projection");
+  glUniformMatrix4fv(projectionLoc, 1, GL_FALSE, &proj[0][0]);
+
+  // Camera Setup
+  // ----
+  vec3 cameraTarget = {0.0f, 0.0f, 0.0f};
+  vec3 cameraDirection = {0.0f, 0.0f, 0.0f}; 
+  glm_vec3_subadd(cameraPos, cameraTarget, cameraDirection);
+  glm_vec3_normalize(cameraDirection);
+  // printf("[%f %f %f]", cameraDirection[0], cameraDirection[1], cameraDirection[2]);
+  //
+  // vec3 up = {0.0f, 1.0f, 0.0};
+  // vec3 cameraRight = GLM_VEC3_ZERO_INIT;
+  // glm_cross(up, cameraDirection, cameraRight);
+  // printf("before norm: [%f %f %f]", cameraRight[0], cameraRight[1], cameraRight[2]);
+  // glm_normalize(cameraRight);
+  // printf("norm: [%f %f %f]", cameraRight[0], cameraRight[1], cameraRight[2]);
+  // glm_cross(cameraDirection, cameraRight, cameraUp);
+  // printf("cam up: [%f %f %f]", cameraUp[0], cameraUp[1], cameraUp[2]);
+
+  // mat2 m2 = GLM_MAT2_IDENTITY_INIT;
+  // pprint_mat2(m2, "m2");
+
 
   glEnable(GL_DEPTH_TEST);
 
@@ -395,6 +468,9 @@ int main(void)
   
   while(!glfwWindowShouldClose(window)) 
   {
+    float currentFrame = (float)(glfwGetTime());
+    deltaTime = currentFrame - lastFrame;
+    lastFrame = currentFrame;
     // input
     processInput(window);
 
@@ -419,11 +495,22 @@ int main(void)
     glUseProgram(s.ID);
     // unsigned int transformLoc = glGetUniformLocation(s.ID, "transform");
     // glUniformMatrix4fv(transformLoc, 1, GL_FALSE, trans[0]);
+    // int viewLoc = glGetUniformLocation(s.ID, "view");
+    // glUniformMatrix4fv(viewLoc, 1, GL_FALSE, &view[0][0]);
+    // int projectionLoc = glGetUniformLocation(s.ID, "projection");
+    // glUniformMatrix4fv(projectionLoc, 1, GL_FALSE, &proj[0][0]);
+    // glm_rotate(model, (float)glfwGetTime() * glm_rad(1.0f), (vec3){0.5f, 1.0f, 0.0f});
+    //
+    // const float radius = 10.0f;
+    // float cam_x = (float)(sin(glfwGetTime()) * radius);
+    // float cam_z = cos(glfwGetTime()) * radius;
+    // glm_lookat((vec3){cam_x, 0.0, cam_z}, (vec3){0.0f, 0.0f, 0.0f},(vec3){0.0f, 1.0f, 0.0f}, view);
+    vec3 target = GLM_VEC3_ZERO_INIT;
+    mat4 view = GLM_MAT4_ZERO_INIT;
+    glm_vec3_add(cameraPos, cameraFront,target);
+    glm_lookat(cameraPos, target, cameraUp, view);
     int viewLoc = glGetUniformLocation(s.ID, "view");
     glUniformMatrix4fv(viewLoc, 1, GL_FALSE, &view[0][0]);
-    int projectionLoc = glGetUniformLocation(s.ID, "projection");
-    glUniformMatrix4fv(projectionLoc, 1, GL_FALSE, &proj[0][0]);
-    // glm_rotate(model, (float)glfwGetTime() * glm_rad(1.0f), (vec3){0.5f, 1.0f, 0.0f});
 
     glBindVertexArray(VAO);
     for(unsigned int i = 0; i < 10; i++)
